01bc18faae4605d62390a6be50f599d912c9e10f
jras <jras@dia.uned.es>
2022-02-11 19:42:00 +0100

    Add option to use plotly instead matplotlib for 3D scatter plot.
    Google Colab [does not support "%matplotlib notebook" for interactive plots][1],
    but [plotly][2] can show [interactive plots in Colab][3].
    
    [1]: https://github.com/googlecolab/colabtools/issues/616#issuecomment-506829877
    [2]: https://plotly.com/python/3d-scatter-plots/
    [3]: https://stackoverflow.com/questions/52859983/

--- clustering_example/visualize_vectors.py
+++ clustering_example/visualize_vectors.py
@@ -4,10 +4,21 @@ from sklearn.decomposition import PCA
 import h5py
 
 
-def visualize(vectors):
+def visualize(vectors, useplotly=False):
     pca = PCA(n_components=3)
     projected_vectors = pca.fit_transform(vectors)
     print(projected_vectors.shape)
+    if useplotly:
+        import plotly.express as px
+        fig = px.scatter_3d(
+            x=projected_vectors[:, 0],
+            y=projected_vectors[:, 1],
+            z=projected_vectors[:, 2],
+        )
+        fig.update_traces(marker_size = 2)
+        fig.show()
+        return
+    # default is to use matplotlib
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     plt.scatter(
@@ -23,10 +34,12 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--vectors", required=True, help="HDF5 file containing the vectors")
+    parser.add_argument("-p", "--useplotly", required=False, action="store_true",
+                        help="Use plotly (in web browser) instead of matplotlib for the 3D scatterplot")
     args = parser.parse_args()
     path = args.vectors
 
     with h5py.File(path, "r") as f:
         vectors = f["vectors"][:]
 
-    visualize(vectors)
+    visualize(vectors, args.useplotly)
